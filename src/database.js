c8cc95ce9e9aacc3503a9f3151df76eab40efbd8
Joseph Salen <jsalen.91@gmail.com>
2021-08-19 16:35:22 -0400

    deleted unnecesary comment

--- src/database.js
+++ src/database.js
@@ -15,58 +15,8 @@ db.mongoose
   })
   .then(() => {
     console.log("Connected to MongoDB");
-    initial();
   })
   .catch((e) => {
     console.error("connection error", e);
     process.exit();
   });
-
-/* mongoose.connect(URI, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
-
-const connection = mongoose.connection;
-
-connection.once("open", () => {
-  console.log("DB connected.");
-}); */
-
-function initial() {
-  Role.estimatedDocumentCount((err, count) => {
-    if (!err && count === 0) {
-      new Role({
-        name: "user",
-      }).save((err) => {
-        if (err) {
-          console.log("error", err);
-        }
-
-        console.log("added 'user' to roles collection");
-      });
-
-      new Role({
-        name: "moderator",
-      }).save((err) => {
-        if (err) {
-          console.log("error", err);
-        }
-
-        console.log("added 'moderator' to roles collection");
-      });
-
-      new Role({
-        name: "admin",
-      }).save((err) => {
-        if (err) {
-          console.log("error", err);
-        }
-
-        console.log("added 'admin' to roles collection");
-      });
-    }
-  });
-}
